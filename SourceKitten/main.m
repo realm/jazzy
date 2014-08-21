c1e7463c57ac0bcd3dd1b912c33977da458c3e03
JP Simard <jp@jpsim.com>
2014-08-20 22:24:26 -0700

    - CLI now shows usage if command cannot be parsed - Added support for generating Swift interfaces for modules

--- SourceKitten/main.m
+++ SourceKitten/main.m
@@ -13,18 +13,60 @@ void sourcekitd_initialize();
 uint64_t sourcekitd_uid_get_from_cstr(const char *);
 xpc_object_t sourcekitd_send_request_sync(xpc_object_t);
 
-int main(int argc, const char * argv[]) {
+void print_usage() {
+    printf("Usage: SourceKitten [--file objc_header_path] [--module module_name --framework_dir /absolute/path/to/framework] [--help]\n");
+}
+
+int generate_swift_interface_for_module(NSString *moduleName, NSString *frameworkDir) {
+
+    sourcekitd_initialize();
+
+    // 1: Build up XPC request to send to SourceKit
+
+    xpc_object_t request = xpc_dictionary_create(NULL, NULL, 0);
+    xpc_dictionary_set_uint64(request, "key.request", sourcekitd_uid_get_from_cstr("source.request.editor.open.interface"));
+    xpc_dictionary_set_string(request, "key.modulename", moduleName.UTF8String);
+    xpc_dictionary_set_string(request, "key.name", [NSString stringWithFormat:@"x-xcode-module://%@", moduleName].UTF8String);
+
+    xpc_object_t compiler_args = xpc_array_create(NULL, 0);
+    xpc_array_set_value(compiler_args, XPC_ARRAY_APPEND, xpc_string_create("-sdk"));
+    xpc_array_set_value(compiler_args, XPC_ARRAY_APPEND, xpc_string_create("/Applications/Xcode6-Beta6.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk"));
+    xpc_array_set_value(compiler_args, XPC_ARRAY_APPEND, xpc_string_create("-F"));
+    xpc_array_set_value(compiler_args, XPC_ARRAY_APPEND, xpc_string_create(frameworkDir.UTF8String));
+    xpc_dictionary_set_value(request, "key.compilerargs", compiler_args);
+
+    // 2: Send the request to SourceKit
+    xpc_object_t result = sourcekitd_send_request_sync(request);
+    if (xpc_get_type(result) != XPC_TYPE_DICTIONARY ||
+        xpc_dictionary_get_count(request) <= 1) {
+        NSLog(@"Error processing SourceKit result");
+        return 1;
+    }
+
+    // 3: Print Swift interface
+    printf("%s", xpc_dictionary_get_string(result, "key.sourcetext"));
+    return 0;
+}
+
+int generate_swift_interface_for_file() {
+
+    NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+    if (arguments.count < 3) {
+        printf("Error: not enough arguments\n\n");
+        print_usage();
+        return 1;
+    }
 
     ////////////////////////////////
     //
-    // 1: Make framework-like structure
+    // Make framework-like structure
     //
     ////////////////////////////////
 
     NSFileManager *fm = [NSFileManager defaultManager];
     NSError *fmError = nil;
 
-    // 1.1: Module name & directory structure
+    // 1: Module name & directory structure
 
     NSString *parentModuleName = [NSString stringWithFormat:@"JazzyModule_%@", [[[NSUUID UUID] UUIDString] stringByReplacingOccurrencesOfString:@"-" withString:@""]];
     NSString *tmpDir = [NSTemporaryDirectory() stringByAppendingPathComponent:@"io.realm.jazzy"];
@@ -48,9 +90,9 @@ int main(int argc, const char * argv[]) {
         return 1;
     }
 
-    // 1.2: Copy Objective-C header file
+    // 2: Copy Objective-C header file
 
-    NSString *objCHeaderFilePath = [[NSProcessInfo processInfo] arguments][1];
+    NSString *objCHeaderFilePath = arguments[2];
     NSString *moduleName = [NSString stringWithFormat:@"%@.%@", parentModuleName, [[objCHeaderFilePath lastPathComponent] stringByDeletingPathExtension]];
     [fm copyItemAtPath:objCHeaderFilePath
                 toPath:[frameworkDir stringByAppendingPathComponent:[NSString stringWithFormat:@"Headers/%@", [objCHeaderFilePath lastPathComponent]]]
@@ -60,51 +102,38 @@ int main(int argc, const char * argv[]) {
         return 1;
     }
 
-    // 1.3: Write umbrella header file
+    // 3: Write umbrella header file
 
     [fm createFileAtPath:[frameworkDir stringByAppendingPathComponent:[NSString stringWithFormat:@"Headers/%@.h", parentModuleName]] contents:[[NSString stringWithFormat:@"#import <%@/%@>", parentModuleName, [objCHeaderFilePath lastPathComponent]] dataUsingEncoding:NSUTF8StringEncoding] attributes:nil];
 
-    // 1.4: Write module map file
+    // 4: Write module map file
 
     [fm createFileAtPath:[frameworkDir stringByAppendingPathComponent:@"Modules/module.modulemap"] contents:[[NSString stringWithFormat:@"framework module %@ { umbrella header \"%@.h\" module * { export * } }", parentModuleName, parentModuleName] dataUsingEncoding:NSUTF8StringEncoding] attributes:nil];
 
-    ////////////////////////////////
-    //
-    // 2: Generate Swift interface using SourceKit
-    //
-    ////////////////////////////////
+    // 5: Generate Swift interface
 
-    sourcekitd_initialize();
+    return generate_swift_interface_for_module(moduleName, tmpDir);
+}
 
-    // 2.1: Build up XPC request to send to SourceKit
+int main(int argc, const char * argv[]) {
 
-    xpc_object_t request = xpc_dictionary_create(NULL, NULL, 0);
-    xpc_dictionary_set_uint64(request, "key.request", sourcekitd_uid_get_from_cstr("source.request.editor.open.interface"));
-    xpc_dictionary_set_string(request, "key.modulename", moduleName.UTF8String);
-    xpc_dictionary_set_string(request, "key.name", [NSString stringWithFormat:@"x-xcode-module://%@", moduleName].UTF8String);
-
-    xpc_object_t compiler_args = xpc_array_create(NULL, 0);
-    xpc_array_set_value(compiler_args, XPC_ARRAY_APPEND, xpc_string_create("-sdk"));
-    xpc_array_set_value(compiler_args, XPC_ARRAY_APPEND, xpc_string_create("/Applications/Xcode6-Beta6.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk"));
-    xpc_array_set_value(compiler_args, XPC_ARRAY_APPEND, xpc_string_create("-F"));
-    xpc_array_set_value(compiler_args, XPC_ARRAY_APPEND, xpc_string_create(tmpDir.UTF8String));
-    xpc_dictionary_set_value(request, "key.compilerargs", compiler_args);
-
-    // 2.2: Send the request to SourceKit
-    xpc_object_t result = sourcekitd_send_request_sync(request);
-    if (xpc_get_type(result) != XPC_TYPE_DICTIONARY ||
-        xpc_dictionary_get_count(request) <= 1) {
-        NSLog(@"Error processing SourceKit result");
+    NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+    if (arguments.count == 1) {
+        printf("Error: not enough arguments\n\n");
+        print_usage();
         return 1;
+    } else if ([arguments[1] isEqualToString:@"--help"]) {
+        print_usage();
+        return 0;
+    } else if ([arguments[1] isEqualToString:@"--file"]) {
+        return generate_swift_interface_for_file();
+    } else if (arguments.count >= 5 &&
+               [arguments[1] isEqualToString:@"--module"] &&
+               [arguments[3] isEqualToString:@"--framework_dir"]) {
+        return generate_swift_interface_for_module(arguments[2], arguments[4]);
     }
 
-    // 2.3: Print to stdout
-    const char *string = xpc_dictionary_get_string(result, "key.sourcetext");
-    [[NSString stringWithCString:string encoding:NSUTF8StringEncoding]
-     writeToFile:@"/dev/stdout"
-     atomically:NO
-     encoding:NSUTF8StringEncoding
-     error:nil];
-
-    return 0;
+    printf("Error: unable to parse command\n\n");
+    print_usage();
+    return 1;
 }
