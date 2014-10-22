705fc3c11eccc9de41666c5962c5118738527b1c
JP Simard <jp@jpsim.com>
2014-10-21 18:40:18 -0700

    major progress

--- lib/jazzy.rb
+++ lib/jazzy.rb
@@ -8,6 +8,16 @@ require "jazzy/gem_version.rb"
 require "jazzy/doc.rb"
 require "jazzy/jazzy_markdown.rb"
 
+# XML Helpers
+
+def xml_attribute(node, name)
+  node.attributes[name].value if node.attributes[name]
+end
+
+def xml_xpath(node, xpath)
+  node.xpath(xpath).text if node.xpath(xpath).text.length > 0
+end
+
 class Jazzy::SourceKitten
   def self.runSourceKitten(arguments)
     bin_path = File.expand_path(File.join(File.dirname(__FILE__), '../bin'))
@@ -17,18 +27,27 @@ class Jazzy::SourceKitten
   def self.parse(sourceKittenOutput)
     xml = Nokogiri::XML(sourceKittenOutput)
     docs = []
-    for child in xml.root.element_children
-      # puts child.inspect
-      # break
+    xml.root.element_children.each do |child|
       doc = Hash.new
       doc[:kind] = child.name
-      doc[:file] = child.attributes["file"].value
-      doc[:line] = child.attributes["line"].value
-      doc[:column] = child.attributes["column"].value if child.attributes["column"]
-      doc[:hasSeparator] = child.attributes["hasSeparator"].value if child.attributes["hasSeparator"]
-      doc[:usr] = child.xpath("USR").text if child.xpath("USR").text.length > 0
-      doc[:name] = child.xpath("Name").text if child.xpath("Name").text.length > 0
-      doc[:declaration] = child.xpath("Declaration").text if child.xpath("Declaration").text.length > 0
+      doc[:file] = xml_attribute(child, "file")
+      doc[:line] = xml_attribute(child, "line")
+      doc[:column] = xml_attribute(child, "column")
+      doc[:hasSeparator] = xml_attribute(child, "hasSeparator")
+      doc[:usr] = xml_xpath(child, "USR")
+      doc[:name] = xml_xpath(child, "Name")
+      doc[:declaration] = xml_xpath(child, "Declaration")
+      doc[:abstract] = xml_xpath(child, "Abstract")
+      doc[:discussion] = xml_xpath(child, "Discussion")
+      doc[:return] = xml_xpath(child, "ResultDiscussion")
+      parameters = []
+      child.xpath("Parameters/Parameter").each do |parameter_el|
+        parameters << {
+          :name => xml_xpath(parameter_el, "Name"),
+          :discussion => $markdown.render(xml_xpath(parameter_el, "Discussion"))
+        }
+      end
+      doc[:parameters] = parameters if parameters
       docs << doc
     end
     docs
@@ -39,36 +58,48 @@ class Jazzy::DocBuilder
   def self.buildDocsForSourceKittenOutput(sourceKittenOutput, outputDir)
     docs = Jazzy::SourceKitten.parse(sourceKittenOutput)
     classes = docs.select { |doc| doc[:kind] == "Class" && doc[:declaration] =~ /class/ }
-
     classes.each do |theClass|
+      classUSR = theClass[:usr]
+      subItems = docs.select { |doc| doc[:usr] != classUSR && doc[:usr] =~ /#{classUSR[3..-1]}/ }
       path = File.join(outputDir, "#{theClass[:name]}.html")
-      File.open(path, 'w') { |file| file.write(Jazzy::DocBuilder.document(theClass)) }
+      File.open(path, 'w') { |file| file.write(Jazzy::DocBuilder.document("Alamofire", theClass, subItems)) }
     end
 
     # Copy assets into output directory
     FileUtils.cp_r(File.expand_path(File.dirname(__FILE__) + '/../lib/jazzy/assets/') + '/.', outputDir)
   end
 
-  def self.document(theClass)
+  def self.document(moduleName, theClass, subItems)
     doc = Jazzy::Doc.new
     doc[:name] = theClass[:name]
-    doc[:kind] = "Class"
-    doc[:overview] = $markdown.render("#{theClass[:name]} is a generic class (roughly the Swift equivalent of `RLMArray`).")
-    taskname = "Creating a #{theClass[:name]}"
-    doc[:tasks] = [
-      {
+    doc[:kind] = theClass[:kind]
+    doc[:overview] = $markdown.render(theClass[:abstract])
+    doc[:tasks] = []
+    tasknames = ["Children"]
+    tasknames.each do |taskname|
+      items = []
+      subItems.each do |subItem|
+        item = {
+          :name => subItem[:name],
+          :abstract => $markdown.render((subItem[:abstract] || "") + (subItem[:discussion] || "")),
+          :declaration => subItem[:declaration],
+          :usr => subItem[:usr]
+        }
+        item[:return] = $markdown.render(subItem[:return]) if subItem[:return]
+        parameters = subItem[:parameters]
+        item[:parameters] = parameters if parameters.length > 0
+        items << item
+      end
+      doc[:tasks] << {
         :name => taskname,
         :uid => URI::encode(taskname),
-        :abstract => $markdown.render("This is the abstract for the constructor."),
-        :declaration => "init(string aString: String)",
-        :parameters => [:name => "aString", :description => $markdown.render("The characters for the new object.")],
-        :return => $markdown.render("a `#{theClass[:name]}` object initialized with the characters of `aString` and no attribute information.")
+        :items => items
       }
-    ]
-    doc[:module_name] = "RealmSwift"
-    doc[:company_name] = "Realm"
-    doc[:company_website] = "http://realm.io"
-    doc[:github_link] = "https://github.com/realm/realm-cocoa"
+    end
+    doc[:module_name] = moduleName
+    doc[:author_name] = moduleName
+    doc[:author_website] = "https://github.com/Alamofire/Alamofire"
+    doc[:github_link] = "https://github.com/Alamofire/Alamofire"
     doc[:dash_link] = "http://kapeli.com/dash"
     doc.render
   end
