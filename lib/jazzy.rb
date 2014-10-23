971dfa0e136022f7e4541959a87283d2e2e0894e
JP Simard <jp@jpsim.com>
2014-10-23 01:28:02 -0700

    preserve doc hierarchy

--- lib/jazzy.rb
+++ lib/jazzy.rb
@@ -1,9 +1,9 @@
-require 'mustache'
-require 'redcarpet'
-require 'nokogiri'
-require 'json'
-require 'date'
-require 'uri'
+require "mustache"
+require "redcarpet"
+require "nokogiri"
+require "json"
+require "date"
+require "uri"
 require "jazzy/gem_version.rb"
 require "jazzy/doc.rb"
 require "jazzy/jazzy_markdown.rb"
@@ -18,9 +18,25 @@ def xml_xpath(node, xpath)
   node.xpath(xpath).text if node.xpath(xpath).text.length > 0
 end
 
+def subUSR(usr)
+  usr.slice((usr.index(/\d/))..-1)
+end
+
+def make_doc_hierarchy(docs, doc)
+  subUSR = subUSR(doc[:usr])
+  docs.each do |hashdoc|
+    hashSubUSR = subUSR(hashdoc[:usr])
+    if subUSR =~ /#{hashSubUSR}/
+      make_doc_hierarchy(hashdoc[:children], doc)
+      return
+    end
+  end
+  docs << doc
+end
+
 class Jazzy::SourceKitten
   def self.runSourceKitten(arguments)
-    bin_path = File.expand_path(File.join(File.dirname(__FILE__), '../bin'))
+    bin_path = File.expand_path(File.join(File.dirname(__FILE__), "../bin"))
     `#{bin_path}/sourcekitten #{(arguments).join(" ")} 2>&1`
   end
 
@@ -40,6 +56,7 @@ class Jazzy::SourceKitten
       doc[:abstract] = xml_xpath(child, "Abstract")
       doc[:discussion] = xml_xpath(child, "Discussion")
       doc[:return] = xml_xpath(child, "ResultDiscussion")
+      doc[:children] = []
       parameters = []
       child.xpath("Parameters/Parameter").each do |parameter_el|
         parameters << {
@@ -50,8 +67,22 @@ class Jazzy::SourceKitten
       doc[:parameters] = parameters if parameters
       docs << doc
     end
-    docs
+
+    # docs are flat at this point. let's unflatten them
+    docs_with_usrs = docs.select { |doc| doc[:usr] != nil }
+    rootToChildSortedDocs = docs_with_usrs.sort { |doc1, doc2| doc1[:usr].length <=> doc2[:usr].length }
+    
+    docs = []
+    rootToChildSortedDocs.each { |doc| make_doc_hierarchy(docs, doc) }
+    sort_docs(docs)
   end
+end
+
+def sort_docs(docs)
+  docs.each do |doc|
+    doc[:children] = sort_docs(doc[:children])
+  end
+  docs.sort { |doc1, doc2| doc1[:line].to_i <=> doc2[:line].to_i }
 end
 
 def prepare_output_dir(output_dir, clean)
@@ -64,18 +95,15 @@ class Jazzy::DocBuilder
     outputDir = options[:output]
     prepare_output_dir(outputDir, options[:clean])
     docs = Jazzy::SourceKitten.parse(sourceKittenOutput)
-    classes = docs.select { |doc| doc[:kind] == "Class" && doc[:declaration] =~ /class/ }
-    classes.each do |theClass|
-      classUSR = theClass[:usr]
-      subItems = docs.select { |doc| doc[:usr] != classUSR && doc[:usr] =~ /#{classUSR[3..-1]}/ }
-      path = File.join(outputDir, "#{theClass[:name]}.html")
-      File.open(path, 'w') { |file| file.write(Jazzy::DocBuilder.document(options, theClass, subItems)) }
+    docs.each do |doc|
+      path = File.join(outputDir, "#{doc[:name]}.html")
+      File.open(path, "w") { |file| file.write(Jazzy::DocBuilder.document(options, doc, doc[:children])) }
     end
 
     # Copy assets into output directory
-    FileUtils.cp_r(File.expand_path(File.dirname(__FILE__) + '/../lib/jazzy/assets/') + '/.', outputDir)
+    FileUtils.cp_r(File.expand_path(File.dirname(__FILE__) + "/../lib/jazzy/assets/") + "/.", outputDir)
 
-    puts "Docs successfully built at " + outputDir
+    puts "jam out ♪♫ to your fresh new docs at " + outputDir
   end
 
   def self.document(options, theClass, subItems)
