b234b9a22ae94e09d2f03ddb3592ac2b0a71674b
JP Simard <jp@jpsim.com>
2014-10-24 16:04:08 -0700

    - added navigation section to left sidebar for top-level types and their children - updated sourcekitten to include <Kind></Kind>

--- lib/jazzy.rb
+++ lib/jazzy.rb
@@ -4,6 +4,7 @@ require "nokogiri"
 require "json"
 require "date"
 require "uri"
+require "active_support/inflector"
 require "jazzy/gem_version.rb"
 require "jazzy/doc.rb"
 require "jazzy/jazzy_markdown.rb"
@@ -34,6 +35,36 @@ def make_doc_hierarchy(docs, doc)
   docs << doc
 end
 
+def kinds
+  {
+    "source.lang.swift.decl.function.method.class" => "Class Method",
+    "source.lang.swift.decl.var.class" => "Class Variable",
+    "source.lang.swift.decl.class" => "Class",
+    "source.lang.swift.decl.var.global" => "Constant",
+    "source.lang.swift.decl.function.constructor" => "Constructor",
+    "source.lang.swift.decl.enumelement" => "Enum Element",
+    "source.lang.swift.decl.enum" => "Enum",
+    "source.lang.swift.decl.function.free" => "Function",
+    "source.lang.swift.decl.function.method.instance" => "Instance Method",
+    "source.lang.swift.decl.var.instance" => "Instance Variable",
+    "source.lang.swift.decl.protocol" => "Protocol",
+    "source.lang.swift.decl.typealias" => "Typealias"
+  }
+end
+
+def group_docs(docs, kind)
+  kindNamePlural = kinds[kind].pluralize
+  group = docs.select { |doc| doc[:kind] == kind }
+  docs = docs.select { |doc| doc[:kind] != kind }
+  docs << {
+    :name => kindNamePlural,
+    :kind => "Overview",
+    :abstract => "The following #{kindNamePlural.downcase} are available globally.",
+    :children => group
+  } if group.count > 0
+  docs
+end
+
 class Jazzy::SourceKitten
   def self.runSourceKitten(arguments)
     bin_path = File.expand_path(File.join(File.dirname(__FILE__), "../bin"))
@@ -47,7 +78,9 @@ class Jazzy::SourceKitten
       next if child.name == "Section" # Skip sections
 
       doc = Hash.new
-      doc[:kind] = child.name
+      doc[:kind] = xml_xpath(child, "Kind")
+      doc[:kindName] = kinds[doc[:kind]]
+      doc[:kindNamePlural] = kinds[doc[:kind]].pluralize
       doc[:file] = xml_attribute(child, "file")
       doc[:line] = xml_attribute(child, "line").to_i
       doc[:column] = xml_attribute(child, "column").to_i
@@ -75,7 +108,12 @@ class Jazzy::SourceKitten
     
     docs = []
     rootToChildSortedDocs.each { |doc| make_doc_hierarchy(docs, doc) }
-    sort_docs(docs)
+    docs = sort_docs(docs)
+    kinds.keys.each do |kind|
+      docs = group_docs(docs, kind)
+    end
+    docs = make_doc_urls(docs, [])
+    docs
   end
 end
 
@@ -86,20 +124,53 @@ def sort_docs(docs)
   docs.sort { |doc1, doc2| doc1[:line] <=> doc2[:line] }
 end
 
+def make_doc_urls(docs, parents)
+  docs.each do |doc|
+    if doc[:children].count > 0
+      parentsSlash = parents.count > 0 ? "/" : ""
+      doc[:url] = parents.join("/") + parentsSlash + doc[:name] + ".html"
+      doc[:children] = make_doc_urls(doc[:children], parents + [doc[:name]])
+    else
+      doc[:url] = parents.join("/") + ".html"
+    end
+  end
+  docs
+end
+
 def prepare_output_dir(output_dir, clean)
   FileUtils.rm_r output_dir if (clean && File.directory?(output_dir))
   FileUtils.mkdir_p output_dir
 end
 
+def doc_structure_for_docs(docs)
+  structure = []
+  docs.each do |doc|
+    structure << {
+      :section => doc[:name],
+      :children => doc[:children].map { |child| {:name => child[:name], :url => child[:url]} }
+    }
+  end
+  structure
+end
+
 class Jazzy::DocBuilder
+  def self.buildDocs(outputDir, docs, options, level, doc_structure)
+    docs.each do |doc|
+      next if doc[:children].count == 0
+      prepare_output_dir(outputDir, false)
+      path = File.join(outputDir, "#{doc[:name]}.html")
+      path_to_root = ['../'].cycle(level).to_a.join("")
+      File.open(path, "w") { |file| file.write(Jazzy::DocBuilder.document(options, doc, path_to_root, doc_structure)) }
+      Jazzy::DocBuilder.buildDocs(File.join(outputDir, doc[:name]), doc[:children], options, level + 1, doc_structure)
+    end
+  end
+
   def self.buildDocsForSourceKittenOutput(sourceKittenOutput, options)
     outputDir = options[:output]
     prepare_output_dir(outputDir, options[:clean])
     docs = Jazzy::SourceKitten.parse(sourceKittenOutput)
-    docs.each do |doc|
-      path = File.join(outputDir, "#{doc[:name]}.html")
-      File.open(path, "w") { |file| file.write(Jazzy::DocBuilder.document(options, doc)) }
-    end
+    doc_structure = doc_structure_for_docs(docs)
+    Jazzy::DocBuilder.buildDocs(outputDir, docs, options, 0, doc_structure)
 
     # Copy assets into output directory
     FileUtils.cp_r(File.expand_path(File.dirname(__FILE__) + "/../lib/jazzy/assets/") + "/.", outputDir)
@@ -107,12 +178,13 @@ class Jazzy::DocBuilder
     puts "jam out ♪♫ to your fresh new docs at " + outputDir
   end
 
-  def self.document(options, docModel)
+  def self.document(options, docModel, path_to_root, doc_structure)
     doc = Jazzy::Doc.new
     doc[:name] = docModel[:name]
-    doc[:kind] = docModel[:kind]
+    doc[:kind] = docModel[:kindName]
     doc[:overview] = $markdown.render(docModel[:abstract])
     doc[:tasks] = []
+    doc[:structure] = doc_structure
     tasknames = ["Children"]
     tasknames.each do |taskname|
       items = []
@@ -129,7 +201,7 @@ class Jazzy::DocBuilder
         items << item
       end
       doc[:tasks] << {
-        :name => taskname,
+        :name => "",
         :uid => URI::encode(taskname),
         :items => items
       }
@@ -139,6 +211,7 @@ class Jazzy::DocBuilder
     doc[:author_website] = options[:authorURL]
     doc[:github_link] = options[:githubURL]
     doc[:dash_link] = options[:dashURL]
+    doc[:path_to_root] = path_to_root
     doc.render
   end
 end
