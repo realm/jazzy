eeff3560d5aa113a326d46c7cce220bbbc505c2d
Samuel E. Giddins <segiddins@segiddins.me>
2014-10-27 14:04:52 -0700

    [RuboCop] Add RuboCop

--- Rakefile
+++ Rakefile
@@ -1,1 +1,59 @@
-require "bundler/gem_tasks"
+#-- Bootstrap --------------------------------------------------------------#
+
+desc 'Initializes your working copy to run the specs'
+task :bootstrap do
+  if system('which bundle')
+    title 'Installing gems'
+    sh 'bundle install'
+  else
+    $stderr.puts "\033[0;31m" \
+      "[!] Please install the bundler gem manually:\n" \
+      '    $ [sudo] gem install bundler' \
+      "\e[0m"
+    exit 1
+  end
+end
+
+begin
+  require 'bundler/gem_tasks'
+
+  task :default => :spec
+
+  #-- Specs ------------------------------------------------------------------#
+
+  desc 'Run specs'
+  task :spec do
+    title 'Running Unit Tests'
+    files = FileList['spec/**/*_spec.rb'].shuffle.join(' ')
+    sh "bundle exec bacon #{files}"
+
+    Rake::Task['rubocop'].invoke
+  end
+
+  #-- RuboCop ----------------------------------------------------------------#
+
+  require 'rubocop/rake_task'
+  RuboCop::RakeTask.new(:rubocop) do |task|
+    task.patterns = %w(lib spec Rakefile Gemfile jazzy.gemspec)
+  end
+
+rescue LoadError, NameError
+  $stderr.puts "\033[0;31m" \
+    '[!] Some Rake tasks haven been disabled because the environment' \
+    ' couldn’t be loaded. Be sure to run `rake bootstrap` first.' \
+    "\e[0m"
+  $stderr.puts e.message
+  $stderr.puts e.backtrace
+  $stderr.puts
+end
+
+#-- Helpers ------------------------------------------------------------------#
+
+def title(title)
+  cyan_title = "\033[0;36m#{title}\033[0m"
+  puts
+  puts '-' * 80
+  puts cyan_title
+  puts '-' * 80
+  puts
+end
--- lib/jazzy/config.rb
+++ lib/jazzy/config.rb
@@ -5,24 +5,24 @@ module Jazzy
     attr_accessor :input
     attr_accessor :output
     attr_accessor :xcodebuild_arguments
-    attr_accessor :authorName
-    attr_accessor :moduleName
-    attr_accessor :githubURL
-    attr_accessor :authorURL
-    attr_accessor :dashURL
+    attr_accessor :author_name
+    attr_accessor :module_name
+    attr_accessor :github_url
+    attr_accessor :author_url
+    attr_accessor :dash_url
     attr_accessor :excludes
     attr_accessor :sourcekitten_sourcefile
     attr_accessor :clean
 
     def initialize
-      self.input = File.expand_path(".")
-      self.output = File.expand_path("docs")
+      self.input = File.expand_path('.')
+      self.output = File.expand_path('docs')
       self.xcodebuild_arguments = []
-      self.authorName = ""
-      self.moduleName = ""
-      self.githubURL = ""
-      self.authorURL = ""
-      self.dashURL = ""
+      self.author_name = ''
+      self.module_name = ''
+      self.github_url = ''
+      self.author_url = ''
+      self.dash_url = ''
       self.excludes = []
       self.sourcekitten_sourcefile = nil
       self.clean = false
@@ -31,58 +31,58 @@ module Jazzy
     def self.parse!
       config = new
       OptionParser.new do |opt|
-        opt.banner = "Usage: jazzy"
-        opt.separator  ""
-        opt.separator  "Options"
+        opt.banner = 'Usage: jazzy'
+        opt.separator ''
+        opt.separator 'Options'
 
-        opt.on("-o", "--output FOLDER", "Folder to output the HTML docs to") do |output|
+        opt.on('-o', '--output FOLDER', 'Folder to output the HTML docs to') do |output|
           config.output = File.expand_path(output)
         end
 
-        opt.on("-e", "--exclude filepath1,filepath2,…filepathN", Array, "Exclude specific files") do |e|
+        opt.on('-e', '--exclude filepath1,filepath2,…filepathN', Array, 'Exclude specific files') do |e|
           config.excludes = e
         end
 
-        opt.on("-c", "--[no-]clean",
-          "Delete contents of output directory before running.",
-          "WARNING: If --output is set to ~/Desktop, this will delete the ~/Desktop directory.") do |clean|
+        opt.on('-c', '--[no-]clean',
+               'Delete contents of output directory before running.',
+               'WARNING: If --output is set to ~/Desktop, this will delete the ~/Desktop directory.') do |clean|
           config.clean = clean
         end
 
-        opt.on("-x", "--xcodebuild-arguments arg1,arg2,…argN", Array, "Arguments to forward to xcodebuild") do |args|
+        opt.on('-x', '--xcodebuild-arguments arg1,arg2,…argN', Array, 'Arguments to forward to xcodebuild') do |args|
           config.xcodebuild_arguments = args
         end
 
-        opt.on("-a", "--author AUTHOR_NAME", "Name of author to attribute in docs (i.e. Realm)") do |a|
-          config.authorName = a
+        opt.on('-a', '--author AUTHOR_NAME', 'Name of author to attribute in docs (i.e. Realm)') do |a|
+          config.author_name = a
         end
 
-        opt.on("-u", "--author_url URL", "Author URL of this project (i.e. http://realm.io)") do |u|
-          config.authorURL = u
+        opt.on('-u', '--author_url URL', 'Author URL of this project (i.e. http://realm.io)') do |u|
+          config.author_url = u
         end
 
-        opt.on("-m", "--module MODULE_NAME", "Name of module being documented. (i.e. RealmSwift)") do |m|
-          config.moduleName = m
+        opt.on('-m', '--module MODULE_NAME', 'Name of module being documented. (i.e. RealmSwift)') do |m|
+          config.module_name = m
         end
 
-        opt.on("-d", "--dash_url URL", "URL to install docs in Dash (i.e. dash-feed://http%3A%2F%2Fcocoadocs.org%2Fdocsets%2FRealm%2FRealm.xml") do |d|
-          config.dashURL = d
+        opt.on('-d', '--dash_url URL', 'URL to install docs in Dash (i.e. dash-feed://http%3A%2F%2Fcocoadocs.org%2Fdocsets%2FRealm%2FRealm.xml') do |d|
+          config.dash_url = d
         end
 
-        opt.on("-g", "--github_url URL", "GitHub URL of this project (i.e. https://github.com/realm/realm-cocoa)") do |g|
-          config.githubURL = g
+        opt.on('-g', '--github_url URL', 'GitHub URL of this project (i.e. https://github.com/realm/realm-cocoa)') do |g|
+          config.github_url = g
         end
 
-        opt.on("-s", "--sourcekitten-sourcefile FILEPATH", "XML doc file generated from sourcekitten to parse") do |s|
+        opt.on('-s', '--sourcekitten-sourcefile FILEPATH', 'XML doc file generated from sourcekitten to parse') do |s|
           config.sourcekitten_sourcefile = s
         end
 
-        opt.on("-v", "--version", "Print version number") do
-          puts "jazzy version: " + Jazzy::VERSION
+        opt.on('-v', '--version', 'Print version number') do
+          puts 'jazzy version: ' + Jazzy::VERSION
           exit
         end
 
-        opt.on("-h", "--help", "Print this help message") do
+        opt.on('-h', '--help', 'Print this help message') do
           puts opt_parser
           exit
         end
