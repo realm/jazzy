5bdd572614117739ea70239baee9d8d21e19648e
JP Simard <jp@jpsim.com>
2014-10-30 13:02:42 -0700

    Initial Commit

---
+++ spec/integration_specs/misc_jazzy_features/MiscJazzyFeatures/MiscJazzyFeatures/MiscJazzyFeatures.h
@@ -0,0 +1,19 @@
+//
+//  MiscJazzyFeatures.h
+//  MiscJazzyFeatures
+//
+//  Created by JP Simard on 10/30/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MiscJazzyFeatures.
+FOUNDATION_EXPORT double MiscJazzyFeaturesVersionNumber;
+
+//! Project version string for MiscJazzyFeatures.
+FOUNDATION_EXPORT const unsigned char MiscJazzyFeaturesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiscJazzyFeatures/PublicHeader.h>
+
+
