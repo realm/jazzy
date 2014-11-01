a1d9b36769d364d54c3287cfaf8319c9d6fb8c85
JP Simard <jp@jpsim.com>
2014-10-31 23:32:08 -0700

    updated jazzy.sh with version number

--- jazzy.sh
+++ jazzy.sh
@@ -4,4 +4,4 @@ jazzy -m sourcekitten \
 -a "JP Simard" \
 -u https://github.com/jpsim/sourcekitten \
 -g https://github.com/jpsim/sourcekitten \
---github-file-prefix https://github.com/jpsim/sourcekitten/blob/0.1.0
+--github-file-prefix https://github.com/jpsim/sourcekitten/blob/0.1.2
