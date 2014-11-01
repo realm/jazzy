fc3e90a14b3d76458815712d2eccb5b8b1c64a9d
JP Simard <jp@jpsim.com>
2014-10-31 20:26:18 -0700

    Added jazzy.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 xcodebuild CONFIGURATION_BUILD_DIR='${PWD}/build/'
 cp ./build/sourcekitten /usr/local/bin/sourcekitten
---
+++ jazzy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+jazzy -m sourcekitten \
+-a "JP Simard" \
+-u https://github.com/jpsim/sourcekitten \
+-g https://github.com/jpsim/sourcekitten \
+--github-file-prefix https://github.com/jpsim/sourcekitten/blob/0.1.0
