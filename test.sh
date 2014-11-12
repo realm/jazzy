3f6c5a99d1af9ab6d3c418a5cffbb66bf6c4c0f7
JP Simard <jp@jpsim.com>
2014-11-11 16:36:32 -0800

    added doc test

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ sh install.sh
 # Test Syntax Text
 
 syntax_text_result="$(sourcekitten --syntax-text 'import Foundation // Hello World!')"
-syntax_text_expected="$(cat tests/syntax_text.txt)"
+syntax_text_expected="$(cat tests/syntax_text.json)"
 if [ "$syntax_text_result" == "$syntax_text_expected" ]; then
     echo "syntax_text passed"
 else
@@ -18,8 +18,8 @@ fi
 
 # Test Syntax
 
-echo 'import Foundation // Hello World' > syntax.txt
-syntax_result="$(sourcekitten --syntax ${CURRENT_PATH}/syntax.txt)"
+echo 'import Foundation // Hello World' > syntax.swift
+syntax_result="$(sourcekitten --syntax ${CURRENT_PATH}/syntax.swift)"
 if [ "$syntax_result" == "$syntax_text_expected" ]; then
     echo "syntax passed"
 else
@@ -27,13 +27,13 @@ else
     echo "$syntax_result"
     exit 1
 fi
-rm syntax.txt
+rm syntax.swift
 
 # Test Structure
 
-echo 'class MyClass { var variable = 0 }' > structure.txt
-structure_result="$(sourcekitten --structure ${CURRENT_PATH}/structure.txt | jsonlint -s)"
-structure_expected="$(cat tests/structure.txt | jsonlint -s)"
+echo 'class MyClass { var variable = 0 }' > structure.swift
+structure_result="$(sourcekitten --structure ${CURRENT_PATH}/structure.swift | jsonlint -s)"
+structure_expected="$(cat tests/structure.json | jsonlint -s)"
 if [ "$structure_result" == "$structure_expected" ]; then
     echo "structure passed"
 else
@@ -41,4 +41,16 @@ else
     echo "$structure_result"
     exit 1
 fi
-rm structure.txt
+rm structure.swift
+
+# Test Bicycle
+
+bicycle_result="$(sourcekitten --single-file ${CURRENT_PATH}/tests/Bicycle.swift -j4 ${CURRENT_PATH}/tests/Bicycle.swift | jsonlint -s)"
+bicycle_expected="$(cat tests/Bicycle.json | jsonlint -s)"
+if [ "$bicycle_result" == "$bicycle_expected" ]; then
+    echo "bicycle passed"
+else
+    echo "bicycle failed"
+    echo "$bicycle_result"
+    exit 1
+fi
---
+++ tests/Bicycle.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+/// 🚲 A two-wheeled, human-powered mode of transportation.
+class Bicycle {
+    /**
+        Frame and construction style.
+
+        - Road: For streets or trails.
+        - Touring: For long journeys.
+        - Cruiser: For casual trips around town.
+        - Hybrid: For general-purpose transportation.
+    */
+    public enum Style {
+        case Road, Touring, Cruiser, Hybrid
+    }
+
+    /**
+        Mechanism for converting pedal power into motion.
+
+        - Fixed: A single, fixed gear.
+        - Freewheel: A variable-speed, disengageable gear.
+    */
+    public enum Gearing {
+        case Fixed
+        case Freewheel(speeds: Int)
+    }
+
+    /**
+        Hardware used for steering.
+
+        - Riser: A casual handlebar.
+        - Café: An upright handlebar.
+        - Drop: A classic handlebar.
+        - Bullhorn: A powerful handlebar.
+    */
+    enum Handlebar {
+        case Riser, Café, Drop, Bullhorn
+    }
+
+    /// The style of the bicycle.
+    let style: Style
+
+    /// The gearing of the bicycle.
+    let gearing: Gearing
+
+    /// The handlebar of the bicycle.
+    let handlebar: Handlebar
+
+    /// The size of the frame, in centimeters.
+    let frameSize: Int
+
+    /// The number of trips travelled by the bicycle.
+    private(set) var numberOfTrips: Int
+
+    /// The total distance travelled by the bicycle, in meters.
+    private(set) var distanceTravelled: Double
+
+    /**
+        Initializes a new bicycle with the provided parts and specifications.
+
+        :param: style The style of the bicycle
+        :param: gearing The gearing of the bicycle
+        :param: handlebar The handlebar of the bicycle
+        :param: centimeters The frame size of the bicycle, in centimeters
+
+        :returns: A beautiful, brand-new, custom built just for you.
+    */
+    init(style: Style, gearing: Gearing, handlebar: Handlebar, frameSize centimeters: Int) {
+        self.style = style
+        self.gearing = gearing
+        self.handlebar = handlebar
+        self.frameSize = centimeters
+
+        self.numberOfTrips = 0
+        self.distanceTravelled = 0.0
+    }
+
+    /**
+        Take a bike out for a spin.
+
+        :param: meters The distance to travel in meters.
+    */
+    func travel(distance meters: Double) {
+        if meters > 0.0 {
+            self.distanceTravelled += meters
+            self.numberOfTrips++
+        }
+    }
+}
