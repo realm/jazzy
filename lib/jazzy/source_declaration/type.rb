d0866572ef12cdd45b5343758c04c1be4f1fbece
Samuel E. Giddins <segiddins@segiddins.me>
2014-12-12 16:26:53 -0800

    [Rubocop] Autocorrect

--- lib/jazzy/source_declaration/type.rb
+++ lib/jazzy/source_declaration/type.rb
@@ -3,89 +3,6 @@ require 'active_support/inflector'
 module Jazzy
   class SourceDeclaration
     class Type
-      TYPES = {
-        'source.lang.swift.decl.function.method.class' => {
-          :jazzy => 'Class Method',
-          :dash => 'Method',
-        }.freeze,
-        'source.lang.swift.decl.var.class' => {
-          :jazzy => 'Class Variable',
-          :dash => 'Variable',
-        }.freeze,
-        'source.lang.swift.decl.class' => {
-          :jazzy => 'Class',
-          :dash => 'Class',
-        }.freeze,
-        'source.lang.swift.decl.function.constructor' => {
-          :jazzy => 'Constructor',
-          :dash => 'Constructor',
-        }.freeze,
-        'source.lang.swift.decl.function.destructor' => {
-          :jazzy => 'Destructor',
-          :dash => 'Method',
-        }.freeze,
-        'source.lang.swift.decl.var.global' => {
-          :jazzy => 'Global Variable',
-          :dash => 'Global',
-        }.freeze,
-        'source.lang.swift.decl.enumelement' => {
-          :jazzy => 'Enum Element',
-          :dash => 'Element',
-        }.freeze,
-        'source.lang.swift.decl.enum' => {
-          :jazzy => 'Enum',
-          :dash => 'Enum',
-        }.freeze,
-        'source.lang.swift.decl.extension' => {
-          :jazzy => 'Extension',
-          :dash => 'Extension',
-        }.freeze,
-        'source.lang.swift.decl.function.free' => {
-          :jazzy => 'Function',
-          :dash => 'Function',
-        }.freeze,
-        'source.lang.swift.decl.function.method.instance' => {
-          :jazzy => 'Instance Method',
-          :dash => 'Method',
-        }.freeze,
-        'source.lang.swift.decl.var.instance' => {
-          :jazzy => 'Instance Variable',
-          :dash => 'Property',
-        }.freeze,
-        'source.lang.swift.decl.var.local' => {
-          :jazzy => 'Local Variable',
-          :dash => 'Variable',
-        }.freeze,
-        'source.lang.swift.decl.var.parameter' => {
-          :jazzy => 'Parameter',
-          :dash => 'Parameter',
-        }.freeze,
-        'source.lang.swift.decl.protocol' => {
-          :jazzy => 'Protocol',
-          :dash => 'Protocol',
-        }.freeze,
-        'source.lang.swift.decl.function.method.static' => {
-          :jazzy => 'Static Method',
-          :dash => 'Method',
-        }.freeze,
-        'source.lang.swift.decl.var.static' => {
-          :jazzy => 'Static Variable',
-          :dash => 'Variable',
-        }.freeze,
-        'source.lang.swift.decl.struct' => {
-          :jazzy => 'Struct',
-          :dash => 'Struct',
-        }.freeze,
-        'source.lang.swift.decl.function.subscript' => {
-          :jazzy => 'Subscript',
-          :dash => 'Method',
-        }.freeze,
-        'source.lang.swift.decl.typealias' => {
-          :jazzy => 'Typealias',
-          :dash => 'Alias',
-        }.freeze,
-      }.freeze
-
       def self.all
         TYPES.keys.map { |k| new(k) }
       end
@@ -129,6 +46,89 @@ module Jazzy
       def ==(other)
         other && kind == other.kind
       end
+
+      TYPES = {
+        'source.lang.swift.decl.function.method.class' => {
+          jazzy: 'Class Method',
+          dash: 'Method',
+        }.freeze,
+        'source.lang.swift.decl.var.class' => {
+          jazzy: 'Class Variable',
+          dash: 'Variable',
+        }.freeze,
+        'source.lang.swift.decl.class' => {
+          jazzy: 'Class',
+          dash: 'Class',
+        }.freeze,
+        'source.lang.swift.decl.function.constructor' => {
+          jazzy: 'Constructor',
+          dash: 'Constructor',
+        }.freeze,
+        'source.lang.swift.decl.function.destructor' => {
+          jazzy: 'Destructor',
+          dash: 'Method',
+        }.freeze,
+        'source.lang.swift.decl.var.global' => {
+          jazzy: 'Global Variable',
+          dash: 'Global',
+        }.freeze,
+        'source.lang.swift.decl.enumelement' => {
+          jazzy: 'Enum Element',
+          dash: 'Element',
+        }.freeze,
+        'source.lang.swift.decl.enum' => {
+          jazzy: 'Enum',
+          dash: 'Enum',
+        }.freeze,
+        'source.lang.swift.decl.extension' => {
+          jazzy: 'Extension',
+          dash: 'Extension',
+        }.freeze,
+        'source.lang.swift.decl.function.free' => {
+          jazzy: 'Function',
+          dash: 'Function',
+        }.freeze,
+        'source.lang.swift.decl.function.method.instance' => {
+          jazzy: 'Instance Method',
+          dash: 'Method',
+        }.freeze,
+        'source.lang.swift.decl.var.instance' => {
+          jazzy: 'Instance Variable',
+          dash: 'Property',
+        }.freeze,
+        'source.lang.swift.decl.var.local' => {
+          jazzy: 'Local Variable',
+          dash: 'Variable',
+        }.freeze,
+        'source.lang.swift.decl.var.parameter' => {
+          jazzy: 'Parameter',
+          dash: 'Parameter',
+        }.freeze,
+        'source.lang.swift.decl.protocol' => {
+          jazzy: 'Protocol',
+          dash: 'Protocol',
+        }.freeze,
+        'source.lang.swift.decl.function.method.static' => {
+          jazzy: 'Static Method',
+          dash: 'Method',
+        }.freeze,
+        'source.lang.swift.decl.var.static' => {
+          jazzy: 'Static Variable',
+          dash: 'Variable',
+        }.freeze,
+        'source.lang.swift.decl.struct' => {
+          jazzy: 'Struct',
+          dash: 'Struct',
+        }.freeze,
+        'source.lang.swift.decl.function.subscript' => {
+          jazzy: 'Subscript',
+          dash: 'Method',
+        }.freeze,
+        'source.lang.swift.decl.typealias' => {
+          jazzy: 'Typealias',
+          dash: 'Alias',
+        }.freeze,
+      }.freeze
     end
   end
 end
