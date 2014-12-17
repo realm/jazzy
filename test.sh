90ee049d49147314cfd96048ff4979593c1e48db
JP Simard <jp@jpsim.com>
2014-12-17 04:42:26 -0800

    fixed xml test

--- test.sh
+++ test.sh
@@ -63,6 +63,7 @@ fi
 
 MUSICIAN_FILE="${CURRENT_PATH}/tests/JAZMusician.h"
 MUSICIAN_COMMAND="sourcekitten --objc ${MUSICIAN_FILE}"
+ESCAPED_CURRENT_PATH=$(echo ${CURRENT_PATH} | sed 's/\//\\\//g')
 
 doc_result="$(${MUSICIAN_COMMAND} 2> /dev/null | sed s/${ESCAPED_CURRENT_PATH}/sourcekit_path/g)"
 doc_expected="$(cat tests/Musician.xml)"
