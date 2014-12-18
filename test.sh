b9b9a3d49169f5e146de64c90165b20a0fd0528e
Samuel E. Giddins <segiddins@segiddins.me>
2014-12-18 00:10:33 -0300

    Fix test invocation

--- test.sh
+++ test.sh
@@ -43,7 +43,7 @@ rm structure.swift
 
 # Test Documentation Generation
 BICYCLE_FILE="tests/Bicycle.swift"
-BICYCLE_COMMAND="sourcekitten --single-file ${BICYCLE_FILE} -j4 ${BICYCLE_FILE}"
+BICYCLE_COMMAND="sourcekitten --single-file ${BICYCLE_FILE} -j4 $(pwd)${BICYCLE_FILE}"
 ESCAPED_CURRENT_PATH=$(echo $(pwd) | sed 's/\//\\\\\\\//g')
 
 doc_result="$(${BICYCLE_COMMAND} 2> /dev/null | sed s/${ESCAPED_CURRENT_PATH}/sourcekit_path/g | jsonlint -s)"
