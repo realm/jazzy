bc690aae489541291447aced70ee75e9ec3217fd
JP Simard <jp@jpsim.com>
2015-01-09 01:58:19 -0800

    added failing test (commented out)

--- Source/SourceKittenFrameworkTests/ModuleTests.swift
+++ Source/SourceKittenFrameworkTests/ModuleTests.swift
@@ -25,4 +25,24 @@ class ModuleTests: XCTestCase {
         XCTAssertNil(error, "JSON should be propery parsed")
         XCTAssertNotNil(jsonArray, "JSON should be propery parsed")
     }
+
+    // This test usually fails around i=2 because SourceKitService becomes inconsistent after a while.
+    // I'd love to find a work-around for this, but so far nothing has worked.
+    // Help greatly appreciated.
+//    func testSourceKittenReturnsSameResponse() {
+//        var lastResponse = ""
+//        for i in 0..<5 {
+//            autoreleasepool {
+//                println(i)
+//                let sourceKittenModule = Module(xcodeBuildArguments: ["-workspace", "SourceKitten.xcworkspace", "-scheme", "SourceKittenFramework"])!
+//                let docsJSON = sourceKittenModule.docs.description
+//                if i == 0 {
+//                    lastResponse = docsJSON
+//                } else {
+//                    XCTAssertEqual(docsJSON, lastResponse, "current response should match last response")
+//                    lastResponse = docsJSON
+//                }
+//            }
+//        }
+//    }
 }
