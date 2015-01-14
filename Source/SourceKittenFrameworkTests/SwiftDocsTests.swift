0620da85186f0b6c585a801bdd09ee0c1b83e073
JP Simard <jp@jpsim.com>
2015-01-13 17:36:09 -0800

    don't compare JSON strings

--- Source/SourceKittenFrameworkTests/SwiftDocsTests.swift
+++ Source/SourceKittenFrameworkTests/SwiftDocsTests.swift
@@ -15,7 +15,10 @@ class SwiftDocsTests: XCTestCase {
     func testSingleSwiftFileDocs() {
         let docs = SwiftDocs(file: File(path: fixturesDirectory + "Bicycle.swift")!, arguments: ["-j4", fixturesDirectory + "Bicycle.swift"])
         let escapedFixturesDirectory = fixturesDirectory.stringByReplacingOccurrencesOfString("/", withString: "\\/")
-        let comparisonString = (docs.description + "\n").stringByReplacingOccurrencesOfString(escapedFixturesDirectory, withString: "")
-        XCTAssertEqual(comparisonString, File(path: fixturesDirectory + "Bicycle.json")?.contents as String, "should generate expected docs for Swift file")
+        let comparisonString = (docs.description + "\n").stringByReplacingOccurrencesOfString(escapedFixturesDirectory, withString: "") as NSString
+        let expected = File(path: fixturesDirectory + "Bicycle.json")!.contents
+        let actualDocsObject = NSJSONSerialization.JSONObjectWithData(comparisonString.dataUsingEncoding(NSUTF8StringEncoding)!, options: nil, error: nil)! as NSDictionary
+        let expectedDocsObject = NSJSONSerialization.JSONObjectWithData(expected.dataUsingEncoding(NSUTF8StringEncoding)!, options: nil, error: nil)! as NSDictionary
+        XCTAssertEqual(actualDocsObject, expectedDocsObject, "should generate expected docs for Swift file")
     }
 }
