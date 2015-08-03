598722dc9bc4edb8a56e3de19ca163245abfdb83
JP Simard <jp@jpsim.com>
2015-08-02 17:59:54 -0700

    fix issue where code blocked documentation comments would be stripped out

--- Source/SourceKittenFramework/SourceDeclaration.swift
+++ Source/SourceKittenFramework/SourceDeclaration.swift
@@ -17,17 +17,19 @@ Parse XML from `key.doc.full_as_xml` from `cursor.info` request.
 - returns: XML parsed as an `XPCDictionary`.
 */
 public func parseFullXMLDocs(xmlDocs: String) -> XPCDictionary? {
-    let cleanXMLDocs = xmlDocs.stringByReplacingOccurrencesOfString("<rawHTML>",  withString: "")
-                              .stringByReplacingOccurrencesOfString("</rawHTML>", withString: "")
+    let cleanXMLDocs = xmlDocs.stringByReplacingOccurrencesOfString("<rawHTML>", withString: "")
+        .stringByReplacingOccurrencesOfString("</rawHTML>", withString: "")
+        .stringByReplacingOccurrencesOfString("<codeVoice>", withString: "`")
+        .stringByReplacingOccurrencesOfString("</codeVoice>", withString: "`")
     return SWXMLHash.parse(cleanXMLDocs).children.first.map { rootXML in
         var docs = XPCDictionary()
         docs[SwiftDocKey.DocType.rawValue] = rootXML.element?.name
         docs[SwiftDocKey.DocFile.rawValue] = rootXML.element?.attributes["file"]
-        docs[SwiftDocKey.DocLine.rawValue] = rootXML.element?.attributes["line"].map {
-            Int64(($0 as NSString).integerValue)
+        docs[SwiftDocKey.DocLine.rawValue] = rootXML.element?.attributes["line"].flatMap {
+            Int64($0)
         }
-        docs[SwiftDocKey.DocColumn.rawValue] = rootXML.element?.attributes["column"].map {
-            Int64(($0 as NSString).integerValue)
+        docs[SwiftDocKey.DocColumn.rawValue] = rootXML.element?.attributes["column"].flatMap {
+            Int64($0)
         }
         docs[SwiftDocKey.DocName.rawValue] = rootXML["Name"].element?.text
         docs[SwiftDocKey.DocUSR.rawValue] = rootXML["USR"].element?.text
@@ -35,7 +37,7 @@ public func parseFullXMLDocs(xmlDocs: String) -> XPCDictionary? {
         let parameters = rootXML["Parameters"].children
         if parameters.count > 0 {
             docs[SwiftDocKey.DocParameters.rawValue] = parameters.map {
-                return [
+                [
                     "name": $0["Name"].element?.text ?? "",
                     "discussion": childrenAsArray($0["Discussion"]) ?? []
                 ] as XPCDictionary
