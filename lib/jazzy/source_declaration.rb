00318a94e9a79e12bce37da9252da7d33c7e3864
Paul Cantrell <cantrell@pobox.com>
2015-08-13 23:16:10 -0500

    Merge default implementations into protocols

--- lib/jazzy/source_declaration.rb
+++ lib/jazzy/source_declaration.rb
@@ -14,6 +14,7 @@ module Jazzy
     attr_accessor :name
     attr_accessor :declaration
     attr_accessor :abstract
+    attr_accessor :default_impl_abstract
     attr_accessor :discussion
     attr_accessor :return
     attr_accessor :children
