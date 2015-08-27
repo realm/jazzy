0d9a45114d31a230067b605041e0407b363c35f5
JP Simard <jp@jpsim.com>
2015-08-26 19:24:32 -0700

    indentation

--- lib/jazzy/doc.rb
+++ lib/jazzy/doc.rb
@@ -11,15 +11,15 @@ module Jazzy
     self.template_name = 'doc'
 
     def copyright
-        config = Config.instance
-        copyright = config.copyright || (
-            # Fake date is used to keep integration tests consistent
-            date = ENV['JAZZY_FAKE_DATE'] || DateTime.now.strftime('%Y-%m-%d')
-            year = date[0..3]
-            "&copy; #{year} [#{config.author_name}](#{config.author_url}). " \
-            "All rights reserved. (Last updated: #{date})"
-        )
-        Jazzy.copyright_markdown.render(copyright).chomp
+      config = Config.instance
+      copyright = config.copyright || (
+        # Fake date is used to keep integration tests consistent
+        date = ENV['JAZZY_FAKE_DATE'] || DateTime.now.strftime('%Y-%m-%d')
+        year = date[0..3]
+        "&copy; #{year} [#{config.author_name}](#{config.author_url}). " \
+        "All rights reserved. (Last updated: #{date})"
+      )
+      Jazzy.copyright_markdown.render(copyright).chomp
     end
 
     def jazzy_version
