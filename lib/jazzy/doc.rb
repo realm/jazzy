0103e935468bea1a32481ded1196c35528bfcb05
JP Simard <jp@jpsim.com>
2015-09-04 11:03:59 -0700

    keep old copyright-related stuff for backwards compatibility

--- lib/jazzy/doc.rb
+++ lib/jazzy/doc.rb
@@ -10,6 +10,25 @@ module Jazzy
   class Doc < Mustache
     self.template_name = 'doc'
 
+    ############################################################################
+    # TODO: Remove shortly (kept for backwards compatibility)
+    #
+    def date
+      # Fake date is used to keep integration tests consistent
+      ENV['JAZZY_FAKE_DATE'] || DateTime.now.strftime('%Y-%m-%d')
+    end
+
+    def year
+      # Fake date is used to keep integration tests consistent
+      if ENV['JAZZY_FAKE_DATE']
+        ENV['JAZZY_FAKE_DATE'][0..3]
+      else
+        DateTime.now.strftime('%Y')
+      end
+    end
+    #
+    ############################################################################
+
     def copyright
       config = Config.instance
       copyright = config.copyright || (
