36d950a798bca0339c53cc45256170e1bd1e92c2
Esad Hajdarevic <esad@esse.at>
2016-04-13 13:57:53 +0200

    Instead of generating lunr index in jazzy, just emit list of all docs and build index in the theme js. In average case, this results in smaller output with neglectible performance overhead on the client.

--- lib/jazzy/search_builder.rb
+++ lib/jazzy/search_builder.rb
@@ -4,26 +4,19 @@ module Jazzy
   module SearchBuilder
     def self.build(source_module, output_dir)
       decls = source_module.all_declarations.select {|d| d.type && d.name && !d.name.empty? }
-      index = decls.map do |d|
-        {
-          name: d.name,
-          abstract: d.abstract,
-          url: d.url,
-          parent_name: d.parent_in_code && d.parent_in_code.name
-        }
+      index = Hash[decls.map do |d|
+        [ d.url,
+          {
+            name: d.name,
+            abstract: d.abstract && d.abstract.split(/\n/).map(&:strip).first,
+            parent_name: d.parent_in_code && d.parent_in_code.name
+          }.reject {|_,v| v.nil? || v.empty? }
+        ]
       end
-
-      Tempfile.open('jazzy-search') do |f|
+      ]
+      File.open(File.join(output_dir,'search.json'),'w') do |f|
         f.write(index.to_json)
-        f.close
-
-        build_index_js = File.expand_path('../search_builder/build_index.js', __FILE__)
-        dest = File.join(output_dir,'search.json')
-        `node #{build_index_js} #{f.path} #{dest}`
-        # To-do: handle failure
       end
-
-      
     end
   end
 end
