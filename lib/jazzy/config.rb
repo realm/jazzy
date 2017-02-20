f995502a1c1745522405c9bf0ae2f2de935d5f25
Jason Wray <jason@kulturny.com>
2017-02-20 12:56:20 -0500

    Add YAML.safe_load guard for Ruby 2.0.0

--- lib/jazzy/config.rb
+++ lib/jazzy/config.rb
@@ -431,7 +431,12 @@ module Jazzy
     def read_config_file(file)
       case File.extname(file)
         when '.json'         then JSON.parse(File.read(file))
-        when '.yaml', '.yml' then YAML.safe_load(File.read(file))
+        when '.yaml', '.yml' then
+          if YAML.respond_to?('safe_load') # ruby >= 2.1.0
+            YAML.safe_load(File.read(file))
+          else
+            YAML.load(File.read(file))
+          end
         else raise "Config file must be .yaml or .json, but got #{file.inspect}"
       end
     end
