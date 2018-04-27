1df780ec1886db69e1368852482bb69ab4c04d69
JP Simard <jp@jpsim.com>
2018-04-27 11:53:37 -0700

    Use default Swift version for Siesta

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -210,8 +210,9 @@ describe_cli 'jazzy' do
       behaves_like cli_spec 'document_siesta',
                             # Siesta already has Docs/
                             '--output api-docs',
-                            # Use Swift 4.1 rather than the specified 3.0.2
-                            '--swift-version=4.1'
+                            # Use the default Swift version rather than the
+                            # specified 4.0
+                            '--swift-version='
     end
 
     describe 'Creates docs for Swift project with a variety of contents' do
