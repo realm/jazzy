realm/jazzy

commit c205b2d3ed2b91c188677e1f33a705f6b83858e5

Add first-class SPM support

lib/jazzy/sourcekitten.rb
@@ -182,16 +182,25 @@ module Jazzy
       end.select { |x| x }.flatten(1)
     end
 
+    def self.use_spm?(options)
+      options.swift_build_tool == :spm ||
+        (!options.swift_build_tool_configured &&
+         Dir['*.xcodeproj'].empty?)
+    end
+
     # Builds SourceKitten arguments based on Jazzy options
     def self.arguments_from_options(options)
       arguments = ['doc']
-      arguments += if options.objc_mode
-                     objc_arguments_from_options(options)
-                   elsif !options.module_name.empty?
-                     ['--module-name', options.module_name, '--']
-                   else
-                     ['--']
-                   end
+      if options.objc_mode
+        arguments += objc_arguments_from_options(options)
+      else
+        arguments += ['--spm'] if use_spm?(options)
+        if options.module_name_configured
+          arguments += ['--module-name', options.module_name]
+        end
+        arguments += ['--']
+      end
+
       arguments + options.xcodebuild_arguments
     end
 

lib/jazzy/config.rb
@@ -192,6 +192,20 @@ module Jazzy
         end
       end
 
+    SWIFT_BUILD_TOOLS = %w[spm xcodebuild].freeze
+
+    config_attr :swift_build_tool,
+      command_line: "--swift-build-tool #{SWIFT_BUILD_TOOLS.join(' | ')}",
+      description: 'Control whether Jazzy uses Swift Package Manager or '\
+                   'xcodebuild to build the module to be documented.  By '\
+                   'default it uses xcodebuild if there is a .xcodeproj '\
+                   'file in the source directory.',
+      parse: ->(tool) do
+        return tool.to_sym if SWIFT_BUILD_TOOLS.include?(tool)
+        raise "Unsupported swift_build_tool #{tool}, "\
+              "supported values: #{SWIFT_BUILD_TOOLS.join(', ')}"
+      end
+
     # ──────── Metadata ────────
 
     config_attr :author_name,

commit c6249f461319c74ebca56e1a2a548dfbf370e4e8

Pass module name from podspec to sourcekitten
Even if `--podspec` is not set, jazzy looks for a `*.podspec` file
and takes metadata from there if it is not set on the command line.
These pieces of config metadata do not show up as `_configured` so
we have to check their content for validity.

This commit restores historical behavior that was accidentally
broken in c205b2d.

lib/jazzy/sourcekitten.rb
@@ -197,7 +197,7 @@ module Jazzy
         arguments += objc_arguments_from_options(options)
       else
         arguments += ['--spm'] if use_spm?(options)
-        if options.module_name_configured
+        unless options.module_name.empty?
           arguments += ['--module-name', options.module_name]
         end
         arguments += ['--']