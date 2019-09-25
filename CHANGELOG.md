a413970c1b32c7c6877ec59d1f6d1666f39034ee
John Fairhurst <johnfairh@gmail.com>
2019-09-25 12:11:36 +0100

    Release 0.11.2 and update gems

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## Master
+## 0.11.2
 
 ##### Breaking
 
--- lib/jazzy/gem_version.rb
+++ lib/jazzy/gem_version.rb
@@ -1,3 +1,3 @@
 module Jazzy
-  VERSION = '0.11.1'.freeze unless defined? Jazzy::VERSION
+  VERSION = '0.11.2'.freeze unless defined? Jazzy::VERSION
 end
